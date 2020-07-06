9f9de00e9850a9aa8a89062643febaa8f38cd338
TANAHASHI, Jiro(aka jtFuruhata) <jt@do-johodai.ac.jp>
2020-07-06 14:23:32 +0900

    change: unpack sample_c4

---
+++ dist/sample_c4/app.c
@@ -0,0 +1,302 @@
+/**
+ ******************************************************************************
+ ** ファイル名 : app.c
+ **
+ ** 概要 : 二輪差動型ライントレースロボットのTOPPERS/HRP3用Cサンプルプログラム
+ **
+ ** 注記 : sample_c4 (sample_c3にBluetooth通信リモートスタート機能を追加)
+ ******************************************************************************
+ **/
+
+#include "ev3api.h"
+#include "app.h"
+
+#if defined(BUILD_MODULE)
+    #include "module_cfg.h"
+#else
+    #include "kernel_cfg.h"
+#endif
+
+#define DEBUG
+
+#if defined(DEBUG)
+    #define _debug(x) (x)
+#else
+    #define _debug(x)
+#endif
+
+#if defined(MAKE_BT_DISABLE)
+    static const int _bt_enabled = 0;
+#else
+    static const int _bt_enabled = 1;
+#endif
+
+/**
+ * シミュレータかどうかの定数を定義します
+ */
+#if defined(MAKE_SIM)
+    static const int _SIM = 1;
+#elif defined(MAKE_EV3)
+    static const int _SIM = 0;
+#else
+    static const int _SIM = 0;
+#endif
+
+/**
+ * 左コース/右コース向けの設定を定義します
+ * デフォルトは左コース(ラインの右エッジをトレース)です
+ */
+#if defined(MAKE_RIGHT)
+    static const int _LEFT = 0;
+    #define _EDGE -1
+#else
+    static const int _LEFT = 1;
+    #define _EDGE 1
+#endif
+
+/**
+ * センサー、モーターの接続を定義します
+ */
+static const sensor_port_t
+    touch_sensor    = EV3_PORT_1,
+    color_sensor    = EV3_PORT_2,
+    sonar_sensor    = EV3_PORT_3,
+    gyro_sensor     = EV3_PORT_4;
+
+static const motor_port_t
+    left_motor      = EV3_PORT_C,
+    right_motor     = EV3_PORT_B;
+
+static int      bt_cmd = 0;     /* Bluetoothコマンド 1:リモートスタート */
+static FILE     *bt = NULL;     /* Bluetoothファイルハンドル */
+
+/* 下記のマクロは個体/環境に合わせて変更する必要があります */
+/* sample_c1マクロ */
+#define LIGHT_WHITE  23         /* 白色の光センサ値 */
+#define LIGHT_BLACK  0          /* 黒色の光センサ値 */
+/* sample_c2マクロ */
+#define SONAR_ALERT_DISTANCE 30 /* 超音波センサによる障害物検知距離[cm] */
+/* sample_c4マクロ */
+//#define DEVICE_NAME     "ET0"  /* Bluetooth名 sdcard:\ev3rt\etc\rc.conf.ini LocalNameで設定 */
+//#define PASS_KEY        "1234" /* パスキー    sdcard:\ev3rt\etc\rc.conf.ini PinCodeで設定 */
+#define CMD_START         '1'    /* リモートスタートコマンド */
+
+/* LCDフォントサイズ */
+#define CALIB_FONT (EV3_FONT_SMALL)
+#define CALIB_FONT_WIDTH (6/*TODO: magic number*/)
+#define CALIB_FONT_HEIGHT (8/*TODO: magic number*/)
+
+/* 関数プロトタイプ宣言 */
+static int sonar_alert(void);
+static void _syslog(int level, char* text);
+static void _log(char* text);
+//static void tail_control(signed int angle);
+//static void backlash_cancel(signed char lpwm, signed char rpwm, int32_t *lenc, int32_t *renc);
+
+/* メインタスク */
+void main_task(intptr_t unused)
+{
+    signed char forward;      /* 前後進命令 */
+    signed char turn;         /* 旋回命令 */
+    //signed char pwm_L, pwm_R; /* 左右モーターPWM出力 */
+
+    /* LCD画面表示 */
+    ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
+
+    _log("HackEV sample_c4");
+    if (_LEFT)  _log("Left course:");
+    else        _log("Right course:");
+
+    /* センサー入力ポートの設定 */
+    ev3_sensor_config(sonar_sensor, ULTRASONIC_SENSOR);
+    ev3_sensor_config(color_sensor, COLOR_SENSOR);
+    ev3_color_sensor_get_reflect(color_sensor); /* 反射率モード */
+    ev3_sensor_config(touch_sensor, TOUCH_SENSOR);
+    /* モーター出力ポートの設定 */
+    ev3_motor_config(left_motor, LARGE_MOTOR);
+    ev3_motor_config(right_motor, LARGE_MOTOR);
+
+    if (_bt_enabled)
+    {
+        /* Open Bluetooth file */
+        bt = ev3_serial_open_file(EV3_SERIAL_BT);
+        assert(bt != NULL);
+
+        /* Bluetooth通信タスクの起動 */
+        act_tsk(BT_TASK);
+    }
+
+    ev3_led_set_color(LED_ORANGE); /* 初期化完了通知 */
+
+    _log("Go to the start, ready?");
+    if (_SIM)   _log("Hit SPACE bar to start");
+    else        _log("Tap Touch Sensor to start");
+
+    if (_bt_enabled)
+    {
+        fprintf(bt, "Bluetooth Remote Start: Ready.\n", EV3_SERIAL_BT);
+        fprintf(bt, "send '1' to start\n", EV3_SERIAL_BT);
+    }
+
+    /* スタート待機 */
+    while(1)
+    {
+        //tail_control(TAIL_ANGLE_STAND_UP); /* 完全停止用角度に制御 */
+
+        if (bt_cmd == 1)
+        {
+            break; /* リモートスタート */
+        }
+
+        if (ev3_touch_sensor_is_pressed(touch_sensor) == 1)
+        {
+            break; /* タッチセンサが押された */
+        }
+
+        tslp_tsk(10 * 1000U); /* 10msecウェイト */
+    }
+
+    /* 走行モーターエンコーダーリセット */
+    ev3_motor_reset_counts(left_motor);
+    ev3_motor_reset_counts(right_motor);
+
+    ev3_led_set_color(LED_GREEN); /* スタート通知 */
+
+    /**
+    * Main loop
+    */
+    while(1)
+    {
+        if (ev3_button_is_pressed(BACK_BUTTON)) break;
+
+        if (sonar_alert() == 1) /* 障害物検知 */
+        {
+            forward = turn = 0; /* 障害物を検知したら停止 */
+        }
+        else
+        {
+            forward = 30; /* 前進命令 */
+            if (ev3_color_sensor_get_reflect(color_sensor) >= (LIGHT_WHITE + LIGHT_BLACK)/2)
+            {
+                turn = -80 * _EDGE; /* 右旋回命令　(右コースは逆) */
+            }
+            else
+            {
+                turn =  80 * _EDGE; /* 左旋回命令　(右コースは逆) */
+            }
+        }
+
+        /* 左右モータでロボットのステアリング操作を行う */
+        ev3_motor_steer(
+            left_motor,
+            right_motor,
+            (int)forward,
+            (int)turn
+        );
+
+        tslp_tsk(4 * 1000U); /* 4msec周期起動 */
+    }
+    ev3_motor_stop(left_motor, false);
+    ev3_motor_stop(right_motor, false);
+
+    if (_bt_enabled)
+    {
+        ter_tsk(BT_TASK);
+        fclose(bt);
+    }
+
+    ext_tsk();
+}
+
+//*****************************************************************************
+// 関数名 : sonar_alert
+// 引数 : 無し
+// 返り値 : 1(障害物あり)/0(障害物無し)
+// 概要 : 超音波センサによる障害物検知
+//*****************************************************************************
+static int sonar_alert(void)
+{
+    static unsigned int counter = 0;
+    static int alert = 0;
+
+    signed int distance;
+
+    if (++counter == 40/4) /* 約40msec周期毎に障害物検知  */
+    {
+        /*
+         * 超音波センサによる距離測定周期は、超音波の減衰特性に依存します。
+         * NXTの場合は、40msec周期程度が経験上の最短測定周期です。
+         * EV3の場合は、要確認
+         */
+        distance = ev3_ultrasonic_sensor_get_distance(sonar_sensor);
+        if ((distance <= SONAR_ALERT_DISTANCE) && (distance >= 0))
+        {
+            alert = 1; /* 障害物を検知 */
+        }
+        else
+        {
+            alert = 0; /* 障害物無し */
+        }
+        counter = 0;
+    }
+
+    return alert;
+}
+
+//*****************************************************************************
+// 関数名 : bt_task
+// 引数 : unused
+// 返り値 : なし
+// 概要 : Bluetooth通信によるリモートスタート。 Tera Termなどのターミナルソフトから、
+//       ASCIIコードで1を送信すると、リモートスタートする。
+//*****************************************************************************
+void bt_task(intptr_t unused)
+{
+    while(1)
+    {
+        if (_bt_enabled)
+        {
+            uint8_t c = fgetc(bt); /* 受信 */
+            switch(c)
+            {
+            case '1':
+                bt_cmd = 1;
+                break;
+            default:
+                break;
+            }
+            fputc(c, bt); /* エコーバック */
+        }
+    }
+}
+
+//*****************************************************************************
+// 関数名 : _syslog
+// 引数 :   int   level - SYSLOGレベル
+//          char* text  - 出力文字列
+// 返り値 : なし
+// 概要 : SYSLOGレベルlebelのログメッセージtextを出力します。
+//        SYSLOGレベルはRFC3164のレベル名をそのまま（ERRだけはERROR）
+//        `LOG_WARNING`の様に定数で指定できます。
+//*****************************************************************************
+static void _syslog(int level, char* text){
+    static int _log_line = 0;
+    if (_SIM)
+    {
+        syslog(level, text);
+    }
+    else
+    {
+        ev3_lcd_draw_string(text, 0, CALIB_FONT_HEIGHT*_log_line++);
+    }
+}
+
+//*****************************************************************************
+// 関数名 : _log
+// 引数 :   char* text  - 出力文字列
+// 返り値 : なし
+// 概要 : SYSLOGレベルNOTICEのログメッセージtextを出力します。
+//*****************************************************************************
+static void _log(char *text){
+    _syslog(LOG_NOTICE, text);
+}

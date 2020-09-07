a5a7bba0c91a8ccfb58ce2f9c572c722912397da
TANAHASHI, Jiro (aka jtFuruhata) <jt@do-johodai.ac.jp>
2020-09-08 05:55:13 +0900

    fix: can't update sim, try `update sim 2020_n.n`

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -43,7 +43,7 @@ if [ "$1" = "update" ]; then
     if [ "$option" = "sim" ]; then
         rm -f "$ETROBO_CACHE/"etrobosim*
         rm -f "$ETROBO_ROOT"/dist/etrobosim*.manifest
-        ls "$ETROBO_ROOT"/dist/*etrobosim*.tar.gz | grep -v "2020_0.992" | while read line; do
+        ls "$ETROBO_ROOT"/dist/*etrobosim*.tar.gz | grep -v "2020_0.992" | grep -v "$2" | while read line; do
             rm -f "$line"
         done
     fi

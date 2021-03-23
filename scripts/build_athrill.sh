d382af985a143ff8c054a356fd9d6170191334ff
TANAHASHI, Jiro(aka jtFuruhata) <jt@do-johodai.ac.jp>
2021-03-23 20:51:38 +0900

    add: mruby-ev3rt integration

--- scripts/build_athrill.sh
+++ scripts/build_athrill.sh
@@ -15,16 +15,20 @@
 # https://github.com/toppers/athrill-target-v850e2m/commits/master
 # https://github.com/toppers/ev3rt-athrill-v850e2m/commits/master
 #
-# the ETrobo official certified commit: Ver.2021.03.16a
-ATHRILL_OFFICIAL_COMMIT="9ff42004b1a8b9adaca427c46f653dbde6ff0f56"
+# the ETrobo official certified commit: Ver.2021.03.21a
+ATHRILL_OFFICIAL_COMMIT="aaec68572691c4e37d4f343f4b4af499c2a555c4"
 TARGET_OFFICIAL_COMMIT="8268f73c32a632b979b6d4d808b64a3ec9f38e47"
-SAMPLE_OFFICIAL_COMMIT="9043a8ba1171b85c66d0d4abae213ab860611587"
-#ATHRILL_HOTFIX_COMMIT=""
-#ATHRILL_HOTFIX_PATH=""
-#TARGET_HOTFIX_COMMIT=""
-#TARGET_HOTFIX_PATH=""
-#SAMPLE_HOTFIX_COMMIT=""
-#SAMPLE_HOTFIX_PATH=""
+SAMPLE_OFFICIAL_COMMIT="68cf8f2f897e3f9ed4d03e31a3067cdc98948cd5"
+
+# mruby environment for UnityETroboSim
+# Powered by mruby Forum
+# http://forum.mruby.org/
+#
+# See commits histories:
+# https://github.com/mruby-Forum/mruby-ev3rt/commits/master
+#
+MRUBY_OFFICIAL_COMMIT="e734fbd8a4189df5cdc7406fddaa5a8edf6e7bab"
+ETROBO_MRUBY_VER="2.0.1"   # please remember this version number is specified in etrobopkg
 
 #
 # the Athrill2 default repository
@@ -36,6 +40,8 @@ ATHRILL_BRANCH="master"
 DEV_AUTHOR="ytoi"
 DEV_BRANCH="master"
 
+#
+# show usage
 if [ "$1" = "--help" ]; then
     echo "Usage:"
     echo "  build_athrill.sh show  ... show current author/branch/commit"
@@ -53,8 +59,10 @@ if [ "$1" = "--help" ]; then
     exit 0
 fi
 
-if [ -d "$ETROBO_ROOT/ev3rt-athrill-v850e2m" ]; then
-    cd "$ETROBO_ROOT/ev3rt-athrill-v850e2m"
+#
+# change repositories
+if [ -d "$ETROBO_ATHRILL_EV3RT" ]; then
+    cd "$ETROBO_ATHRILL_EV3RT"
     CURRENT_AUTHOR=`git remote -v | head -n 1 | sed -E "s/^.*github.com\/(.*)\/.*$/\1/"`
     CURRENT_BRANCH=`git branch | grep ^* | sed -E 's/^\*\s(.*)$/\1/'`
     if [ "$CURRENT_BRANCH" = "master" ]; then
@@ -63,7 +71,22 @@ if [ -d "$ETROBO_ROOT/ev3rt-athrill-v850e2m" ]; then
         CURRENT_COMMIT=`echo "$CURRENT_BRANCH" | sed -E 's/^\(HEAD detached at (.*)\)$/\1/'`
         CURRENT_BRANCH="master"
     fi
-    cd "$ETROBO_ROOT/athrill-target-v850e2m"
+    if [ -d "$ETROBO_MRUBY_EV3RT" ]; then
+        cd "$ETROBO_MRUBY_EV3RT"
+        MRUBY_AUTHOR=`git remote -v | head -n 1 | sed -E "s/^.*github.com\/(.*)\/.*$/\1/"`
+        MRUBY_BRANCH=`git branch | grep ^* | sed -E 's/^\*\s(.*)$/\1/'`
+        if [ "$MRUBY_BRANCH" = "master" ]; then
+            MRUBY_COMMIT="HEAD"
+        else
+            MRUBY_COMMIT=`echo "$MRUBY_BRANCH" | sed -E 's/^\(HEAD detached at (.*)\)$/\1/'`
+            MRUBY_BRANCH="master"
+        fi
+    else
+        MRUBY_AUTHOR="NOT"
+        MRUBY_COMMIT="INSTALLED"
+        MRUBY_BRANCH="YET"
+    fi
+    cd "$ETROBO_ATHRILL_TARGET"
     TARGET_AUTHOR=`git remote -v | head -n 1 | sed -E "s/^.*github.com\/(.*)\/.*$/\1/"`
     TARGET_BRANCH=`git branch | grep ^* | sed -E 's/^\*\s(.*)$/\1/'`
     if [ "$TARGET_BRANCH" = "master" ]; then
@@ -85,13 +108,17 @@ else
     unset CURRENT_AUTHOR
 fi
 
+#
+# show status
 if [ "$1" = "show" ]; then
     echo "Current status of this Athrill-related repositories:"
     echo "       athrill: $ATH2_AUTHOR/$ATH2_COMMIT/$ATH2_BRANCH"
     echo "athrill-target: $TARGET_AUTHOR/$TARGET_COMMIT/$TARGET_BRANCH"
     echo " ev3rt-athrill: $CURRENT_AUTHOR/$CURRENT_COMMIT/$CURRENT_BRANCH"
+    echo "   mruby-ev3rt: $MRUBY_AUTHOR/$MRUBY_COMMIT/$MRUBY_BRANCH"
     if [ "$ATH2_AUTHOR/$ATH2_COMMIT/$ATH2_BRANCH" = "$ATHRILL_AUTHOR/${ATHRILL_OFFICIAL_COMMIT:0:7}/$ATHRILL_BRANCH" ] \
     && [ "$TARGET_AUTHOR/$TARGET_COMMIT/$TARGET_BRANCH" = "$ATHRILL_AUTHOR/${TARGET_OFFICIAL_COMMIT:0:7}/$ATHRILL_BRANCH" ] \
+    && [ "$MRUBY_AUTHOR/$MRUBY_COMMIT/$MRUBY_BRANCH" = "mruby-Forum/${TARGET_OFFICIAL_COMMIT:0:7}/master" ] \
     && [ "$CURRENT_AUTHOR/$CURRENT_COMMIT/$CURRENT_BRANCH" = "$ATHRILL_AUTHOR/${SAMPLE_OFFICIAL_COMMIT:0:7}/$ATHRILL_BRANCH" ]; then
         echo "the ETrobo official certified commit"
         exit 0
@@ -101,11 +128,15 @@ if [ "$1" = "show" ]; then
     fi
 fi
 
+#
+# set no build option
 if [ "$1" = "check" ]; then
     CHECK="no build"
     shift
 fi
 
+#
+# make athrill clean
 if [ -z "$CHECK" ]; then
     if [ -d "$ETROBO_ATHRILL_WORKSPACE" ]; then
         cd "$ETROBO_ATHRILL_WORKSPACE"
@@ -120,21 +151,46 @@ fi
 
 cd "$ETROBO_ROOT"
 
+#
+# switch into dev repos
 if [ "$1" = "dev" ]; then
     GIT_AUTHOR=$DEV_AUTHOR
 else
     GIT_AUTHOR=$ATHRILL_AUTHOR
 fi
 
+#
+# clone athrill repositories
 if [ "$GIT_AUTHOR" != "$CURRENT_AUTHOR" ]; then
     rm -rf "$ETROBO_ROOT/athrill"
-    rm -rf "$ETROBO_ROOT/athrill-target-v850e2m"
-    rm -rf "$ETROBO_ROOT/ev3rt-athrill-v850e2m"
+    rm -rf "$ETROBO_ATHRILL_TARGET"
+    rm -rf "$ETROBO_ATHRILL_EV3RT"
     git clone https://github.com/${GIT_AUTHOR}/athrill.git
     git clone https://github.com/${GIT_AUTHOR}/athrill-target-v850e2m.git
     git clone https://github.com/${GIT_AUTHOR}/ev3rt-athrill-v850e2m.git
 fi
 
+#
+# clone and build mruby-ev3rt
+cd "$ETROBO_ATHRILL_WORKSPACE"
+if [ ! -d "$ETROBO_MRUBY_EV3RT" ]; then
+    echo "Download mruby-ev3rt"
+    git clone https://github.com/mruby-Forum/mruby-ev3rt.git
+
+    echo "Build mruby-ev3rt"
+    cp "$ETROBO_ROOT/dist/$ETROBO_MRUBY_VER.tar.gz" ./
+    tar xvf "$ETROBO_MRUBY_VER.tar.gz" >/dev/null 2>&1
+    cd "$ETROBO_MRUBY_EV3RT"
+    cat build_config_ev3rt_sim.rb \
+    | sed -E "s/^EV3RT_PATH\ =\ \"(.*)\"$/EV3RT_PATH = \"\$ETROBO_ATHRILL_EV3RT\"/" \
+    | sed -E "s/^GNU_TOOL_PREFX\ =\ \"(.*)\"$/GNU_TOOL_PREFX = \"\$ETROBO_ATHRILL_GCC\/bin\/v850-elf-\"/" \
+    > build_config_ev3rt_sim_etrobo.rb
+    cd $ETROBO_MRUBY_ROOT
+    MRUBY_CONFIG="$ETROBO_MRUBY_EV3RT/build_config_ev3rt_sim_etrobo.rb" rake
+fi
+
+#
+# checkout specific commits
 if [ -n "$1" ]; then
     cd "$ETROBO_ROOT/athrill"
     git checkout .
@@ -142,9 +198,6 @@ if [ -n "$1" ]; then
     git pull
     if [ "$1" = "official" ]; then
         git checkout $ATHRILL_OFFICIAL_COMMIT
-        if [ -n "$ATHRILL_HOTFIX_COMMIT" ]; then
-            wget "https://github.com/$ATHRILL_AUTHOR/athrill/raw/$ATHRILL_HOTFIX_COMMIT/$ATHRILL_HOTFIX_PATH" -O $ATHRILL_HOTFIX_PATH
-        fi
     fi
     cd ../athrill-target-v850e2m
     git checkout .
@@ -152,9 +205,6 @@ if [ -n "$1" ]; then
     git pull
     if [ "$1" = "official" ]; then
         git checkout $TARGET_OFFICIAL_COMMIT
-        if [ -n "$TARGET_HOTFIX_COMMIT" ]; then
-            wget "https://github.com/$ATHRILL_AUTHOR/athrill-target-v850e2m/raw/$TARGET_HOTFIX_COMMIT/$TARGET_HOTFIX_PATH" -O $TARGET_HOTFIX_PATH
-        fi
     fi
     cd ../ev3rt-athrill-v850e2m
     git checkout .
@@ -162,12 +212,31 @@ if [ -n "$1" ]; then
     git pull
     if [ "$1" = "official" ]; then
         git checkout $SAMPLE_OFFICIAL_COMMIT
-        if [ -n "$SAMPLE_HOTFIX_COMMIT" ]; then
-            wget "https://github.com/$ATHRILL_AUTHOR/ev3rt-athrill-v850e2m/raw/$SAMPLE_HOTFIX_COMMIT/$SAMPLE_HOTFIX_PATH" -O $SAMPLE_HOTFIX_PATH
-        fi
+    fi
+    cd sdk/workspace/mruby-ev3rt
+    git checkout .
+    git checkout master
+    git pull
+    if [ "$1" = "official" ]; then
+        git checkout $MRUBY_OFFICIAL_COMMIT
     fi
 fi
 
+#
+# modify Makefile.inc for mruby
+cd "$ETROBO_ATHRILL_WORKSPACE/base_practice_1_mruby"
+if [ -z "`cat Makefile.inc | grep ETROBO_MRUBY`" ]; then
+    echo "modify \$ETROBO_ATHRILL_WORKSPACE/base_practice_1_mruby/Makefile.inc"
+    cat Makefile.inc \
+    | sed -E "s/^APPL_LIBS\ \+=\ (.*libmruby.a)(.*)$/APPL_LIBS += \$(ETROBO_MRUBY_LIB)\2/" \
+    | sed -E "s/^INCLUDES\ \+=\ -I(.*\/mruby-$ETROBO_MRUBY_VER\/include\/)(.*)$/INCLUDES += -I\$(ETROBO_MRUBY_ROOT)\/include\/\2/" \
+    > Makefile.inc.mod
+    mv -f Makefile.inc.mod Makefile.inc
+    cp -rf "$ETROBO_ATHRILL_WORKSPACE/base_practice_1_mruby" "$ETROBO_HRP3_WORKSPACE/"
+fi
+
+#
+# build athrill
 cd "$ETROBO_ATHRILL_TARGET"
 if [ -z "$CHECK" ]; then
     make etrobo_optimize=true
--- scripts/fakemake.sh
+++ scripts/fakemake.sh
@@ -3,16 +3,23 @@
 # fakemake, integrated build and launch system among HRP3/EV3 and ASP3/Athrill
 #   fakemake.sh 
 # Author: jtFuruhata, mhikichi1769
-# Copyright (c) 2020 ETロボコン実行委員会, Released under the MIT license
+# Copyright (c) 2020-2021 ETロボコン実行委員会, Released under the MIT license
 # See LICENSE
 #
 
 cd "$ETROBO_HRP3_WORKSPACE"
 
-# `make nostrip` strips symbols
-nostrip="nostrip"   # ToDo: debug symbol strip
+# `make skiphrp3` skips building for HRP3
+unset skiphrp3
+if [ "$1" = "skiphrp3" ]; then
+    skiphrp3="$1"
+    shift
+fi
+
+# `make strip` strips symbols
+unset strip
 if [ "$1" = "strip" ]; then
-    unset nostrip
+    strip="$1"
     shift
 fi
 
@@ -71,15 +78,10 @@ fi
 if [ "$1" = "sample" ]; then
     if [ "$2" = "tr" ]; then
         cd "$ETROBO_ROOT"
-        make $zip $import $courseSelect $manual_launch $unprefs $btcat app="etrobo_tr" sim up
+        make $skiphrp3 $strip $import $courseSelect $manual_launch $unprefs $btcat app="etrobo_tr" sim up
     else
         cd "$ETROBO_ROOT"
-        make $zip $import $courseSelect $manual_launch $unprefs $btcat app="sample_c4" sim up
-#        cd "$ETROBO_ATHRILL_WORKSPACE"
-#        make $courseSelect img=athrillsample
-#        if [ $? -eq 0 ]; then
-#            sim launchws
-#        fi
+        make $skiphrp3 $strip $import $courseSelect $manual_launch $unprefs $btcat app="sample_c4" sim up
     fi
     exit 0
 fi
@@ -105,14 +107,24 @@ if [ "$1" = "stop" ]; then
     exit 0
 fi
 
-# prepare current app and get project name
+# parse options and get project name
+unset build_asp3
+unset simopt
 args="$@"
 for arg in "$@"; do
     prepare=`echo "$arg" | grep -e app= -e img= | sed -E "s/^app=|img=(.*)$/\1/"`
     if [ -n "$prepare" ]; then
         proj="$prepare"
     fi
+    if [ "$arg" == "sim" ]; then
+        build_asp3="sim"
+    elif [ "$arg" == "start" ]; then
+        simopt="start"
+    elif [ "$arg" == "up" ]; then
+        simopt="up"
+    fi
 done
+# prepare current app
 if [ -z "$proj" ] && [ -f currentapp ]; then
     currentapp=`head -n 1 currentapp`
     args="$args $currentapp"
@@ -135,86 +147,99 @@ if [ -n "$copts" ]; then
 fi
 cp -f "$incFile" "${incFile}.base"
 
-# invoke make for HRP3/EV3
-echo >> "$incFile"
-echo "COPTS += -DMAKE_EV3" >> "$incFile"
-echo invoker make $arg_app_prefix $args
-make $arg_app_prefix $args
-makeResult=$?
-cp -f "${incFile}.org" "$incFile"
-if [ $makeResult -eq 0 ]; then
-    echo fakemake on HRP3: build succeed: ${app_prefix}${proj}
-    currentapp=`head -n 1 currentapp`
-    simopt=`tail -n 1 currentapp`
-
-    if [ "$currentapp" != "$simopt" ]; then
-        if [ "$proj" = "athrillsample" ]; then
-            echo fakemake on ASP3: \"athrillsample\" can\'t be integrated on this system.
-            echo please make on \$ETROBO_ATHRILL_WORKSPACE... I can\'t remember there absolute path...
-            exit 1
-        fi
-
-        # invoke make for ASP3/Athrill
-        rm -rf "$ETROBO_ATHRILL_WORKSPACE/$proj"
-        mv -f "${incFile}.base" "${incFile}"
-        cp -rH "$ETROBO_HRP3_WORKSPACE/$proj" "$ETROBO_ATHRILL_WORKSPACE/"  # Issue #30: thx to @Amakuchisan
-        mv -f "${incFile}.org" "$incFile"
-
-        cd "$ETROBO_ATHRILL_WORKSPACE"
-        echo >> "$incFile"
-        echo "COPTS += -DMAKE_SIM" >> "$incFile"
-        make img="$proj"
-        if [ $? -eq 0 ]; then
-            mv -f "${incFile}.org" "$incFile"
-            if [ -z "$nostrip" ]; then
-                echo fakemake on ASP3: strip debug symbols
-                v850-elf-strip --keep-symbol=_athrill_device_func_call --keep-symbol=_athrill_device_raise_interrupt asp
-            fi
-            echo fakemake on ASP3: build succeed: ${app_prefix}${proj}.asp
-            cp -f asp "${app_prefix}${proj}.asp"
-            echo "${app_prefix}${proj}.asp" > currentasp
-
-            #
-            # prepare simdist folder
-            #
-            # the directory structure for new launch simdist procedure:
-            # `sim` launches athrill apps from under the `workspace/simdist/[projName]` folder.
-            #
-            # $ETROBO_ATHRILL_WORKSPACE
-            #   |- athrill2
-            # $ETROBO_HRP3_WORKSPACE
-            #   |- [simdist]
-            #       |- [projName]
-            #           |- log.txt
-            #           |- l_projName.asp
-            #           |- r_projName.asp
-            #           |- settings.json
-            #           |- __ev3rt_bt_in
-            #           |- __ev3rt_bt_out
-            #           |- [__ev3rtfs]
-            #
-            simdist="$ETROBO_SIM_DIST/$proj"
-            if [ ! -d "$simdist" ]; then
-                mkdir -p "$simdist"
-            fi
-            cp -f "${app_prefix}${proj}.asp" "$simdist/"
-
-            if [ "$simopt" = "up" ]; then
-                echo "launch sim"
-            	sim $export $courseSelect $manual_launch $unprefs $btcat launch $proj
-            elif [ "$simopt" = "start" ]; then
-            	sim $export $courseSelect $manual_launch $unprefs $btcat only launch $proj
-            fi
-        else
-            echo fakemake on ASP3: one or more error occured while build for $proj
-            exit 1
-        fi
-    fi
-    rm -f "${incFile}.org"
-    rm -f "${incFile}.base"
-else
-    echo fakemake on HRP3: make failed ... `cat currentapp`
-    cd "$ETROBO_HRP3_WORKSPACE"
-    rm -f "${incFile}.org"
-    rm -f "${incFile}.base"
+# build to mruby bytecode
+if [ -n "`cat \"$incFile\" | grep ETROBO_MRUBY`" ]; then
+    echo "Build to mruby bytecode"
+    cd "$proj"
+    rm main_task.h
+    "$ETROBO_MRUBY_ROOT/bin/mrbc"  -g -v -Bbcode -omain_task.h main_task.rb
+    cd ..
+    skiphrp3="mruby"
 fi
+
+# invoke make for HRP3/EV3
+echo invoke: make $arg_app_prefix $args
+if [ -z "$skiphrp3" ]; then
+    echo >> "$incFile"
+    echo "COPTS += -DMAKE_EV3" >> "$incFile"
+    make $arg_app_prefix $args
+    makeResult=$?
+    cp -f "${incFile}.org" "$incFile"
+    if [ $makeResult -eq 0 ]; then
+        echo fakemake on HRP3: build succeed: ${app_prefix}${proj}
+        currentapp=`head -n 1 currentapp`
+        simopt=`tail -n 1 currentapp`
+    else
+        echo fakemake on HRP3: make failed ... `cat currentapp`
+        cd "$ETROBO_HRP3_WORKSPACE"
+        rm -f "${incFile}.org"
+        rm -f "${incFile}.base"
+        exit 1
+    fi
+fi
+
+if [ -n "$build_asp3" ]; then
+    if [ "$proj" = "athrillsample" ]; then
+        echo fakemake on ASP3: \"athrillsample\" can\'t be integrated on this system.
+        echo please make on \$ETROBO_ATHRILL_WORKSPACE... I can\'t remember there absolute path...
+        exit 1
+    fi
+
+    # invoke make for ASP3/Athrill
+    rm -rf "$ETROBO_ATHRILL_WORKSPACE/$proj"
+    mv -f "${incFile}.base" "${incFile}"
+    cp -rH "$ETROBO_HRP3_WORKSPACE/$proj" "$ETROBO_ATHRILL_WORKSPACE/"  # Issue #30: thx to @Amakuchisan
+    mv -f "${incFile}.org" "$incFile"
+
+    cd "$ETROBO_ATHRILL_WORKSPACE"
+    echo >> "$incFile"
+    echo "COPTS += -DMAKE_SIM" >> "$incFile"
+    make img="$proj"
+    if [ $? -eq 0 ]; then
+        mv -f "${incFile}.org" "$incFile"
+        if [ -n "$strip" ]; then
+            echo fakemake on ASP3: strip debug symbols
+            v850-elf-strip --keep-symbol=_athrill_device_func_call --keep-symbol=_athrill_device_raise_interrupt asp
+        fi
+        echo fakemake on ASP3: build succeed: ${app_prefix}${proj}.asp
+        cp -f asp "${app_prefix}${proj}.asp"
+        echo "${app_prefix}${proj}.asp" > currentasp
+
+        #
+        # prepare simdist folder
+        #
+        # the directory structure for new launch simdist procedure:
+        # `sim` launches athrill apps from under the `workspace/simdist/[projName]` folder.
+        #
+        # $ETROBO_ATHRILL_WORKSPACE
+        #   |- athrill2
+        # $ETROBO_HRP3_WORKSPACE
+        #   |- [simdist]
+        #       |- [projName]
+        #           |- log.txt
+        #           |- l_projName.asp
+        #           |- r_projName.asp
+        #           |- settings.json
+        #           |- __ev3rt_bt_in
+        #           |- __ev3rt_bt_out
+        #           |- [__ev3rtfs]
+        #
+        simdist="$ETROBO_SIM_DIST/$proj"
+        if [ ! -d "$simdist" ]; then
+            mkdir -p "$simdist"
+        fi
+        cp -f "${app_prefix}${proj}.asp" "$simdist/"
+
+        if [ "$simopt" = "up" ]; then
+            echo "launch sim"
+            sim $export $courseSelect $manual_launch $unprefs $btcat launch $proj
+        elif [ "$simopt" = "start" ]; then
+            sim $export $courseSelect $manual_launch $unprefs $btcat only launch $proj
+        fi
+    else
+        echo fakemake on ASP3: one or more error occured while build for $proj
+        exit 1
+    fi
+fi
+rm -f "${incFile}.org"
+rm -f "${incFile}.base"

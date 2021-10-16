f513d1a588e32de0cce6e2ae657135a80f09e7e4
TANAHASHI, Jiro (aka jtFuruhata) <jt@do-johodai.ac.jp>
2021-10-16 13:17:57 +0900

    fix: WSL_INTEROP probrem

--- scripts/etroboenv.sh
+++ scripts/etroboenv.sh
@@ -2,11 +2,20 @@
 # etrobo environment core
 #   etroboenv.sh 
 # Author: jtFuruhata
-# Copyright (c) 2020 ETロボコン実行委員会, Released under the MIT license
+# Copyright (c) 2020-2021 ETロボコン実行委員会, Released under the MIT license
 # See LICENSE
 #
 
 currentDir="`pwd`"
+
+# against VSCode bug on WSL2
+if [ "$ETROBO_OS_SUBSYSTEM" == "WSL2" ] || [ -n "`uname -r | grep WSL2`" ]; then
+    for process in $(pstree -np -s $$ | grep -o -E '[0-9]+'); do
+        if [[ -e "/run/WSL/${process}_interop" ]]; then
+            export WSL_INTEROP=/run/WSL/${process}_interop
+        fi
+    done
+fi
 
 if [ "$1" = "unset" ]; then
     . sim unset
@@ -38,102 +47,91 @@ else
     if [ "$1" = "silent" ]; then
         quit="no message mode"
     fi
-    if [ ! -f "$ETROBO_ROOT/disable" ]; then
-        export ETROBO_ENV="available"
-        export ETROBO_SCRIPTS="$ETROBO_ROOT/scripts"
-        export ETROBO_ATHRILL_GCC="$ETROBO_ROOT/athrill-gcc-package/usr/local/athrill-gcc"
-        export ETROBO_HRP3_SDK="$ETROBO_ROOT/hrp3/sdk"
-        export ETROBO_HRP3_WORKSPACE="$ETROBO_HRP3_SDK/workspace"
-        export ETROBO_ATHRILL_EV3RT="$ETROBO_ROOT/ev3rt-athrill-v850e2m"
-        export ETROBO_ATHRILL_SDK="$ETROBO_ATHRILL_EV3RT/sdk"
-        export ETROBO_ATHRILL_WORKSPACE="$ETROBO_ATHRILL_SDK/workspace"
-        export ETROBO_MRUBY_EV3RT="$ETROBO_ATHRILL_WORKSPACE/mruby-ev3rt"
-        export ETROBO_MRUBY_VER="2.0.1"
-        export ETROBO_MRUBY_ROOT="$ETROBO_ATHRILL_WORKSPACE/mruby-$ETROBO_MRUBY_VER"
-        export ETROBO_MRUBY_LIB="$ETROBO_MRUBY_ROOT/build/EV3RT-sim/lib/libmruby.a"
-        export ETROBO_SDCARD="$ETROBO_ROOT/ev3rt-1.0-release/sdcard"
+    if [ "$ETROBO_ENV" != "available" ]; then
+        if [ ! -f "$ETROBO_ROOT/disable" ]; then
+            export ETROBO_ENV="available"
+            export ETROBO_SCRIPTS="$ETROBO_ROOT/scripts"
+            export ETROBO_ATHRILL_GCC="$ETROBO_ROOT/athrill-gcc-package/usr/local/athrill-gcc"
+            export ETROBO_HRP3_SDK="$ETROBO_ROOT/hrp3/sdk"
+            export ETROBO_HRP3_WORKSPACE="$ETROBO_HRP3_SDK/workspace"
+            export ETROBO_ATHRILL_EV3RT="$ETROBO_ROOT/ev3rt-athrill-v850e2m"
+            export ETROBO_ATHRILL_SDK="$ETROBO_ATHRILL_EV3RT/sdk"
+            export ETROBO_ATHRILL_WORKSPACE="$ETROBO_ATHRILL_SDK/workspace"
+            export ETROBO_MRUBY_EV3RT="$ETROBO_ATHRILL_WORKSPACE/mruby-ev3rt"
+            export ETROBO_MRUBY_VER="2.0.1"
+            export ETROBO_MRUBY_ROOT="$ETROBO_ATHRILL_WORKSPACE/mruby-$ETROBO_MRUBY_VER"
+            export ETROBO_MRUBY_LIB="$ETROBO_MRUBY_ROOT/build/EV3RT-sim/lib/libmruby.a"
+            export ETROBO_SDCARD="$ETROBO_ROOT/ev3rt-1.0-release/sdcard"
 
-        export ETROBO_PLATFORM="`uname -m`"
-        subsystem="`uname -r | sed -E 's/^.*-(.*)$/\1/'`"
-        if [ `uname` == "Darwin" ]; then
-            export ETROBO_OS="mac"
-            export ETROBO_KERNEL="darwin"
-            export ETROBO_KERNEL_POSTFIX="mac"
-            export ETROBO_USERPROFILE="$HOME_ORG"
-            export ETROBO_EXE_POSTFIX=".app"
-        elif [ "$subsystem" == "Microsoft" ] || [ "$subsystem" == "WSL2" ]; then
-            if [ "$subsystem" == "Microsoft" ]; then
-                subsystem="WSL1"
+            export ETROBO_PLATFORM="`uname -m`"
+            subsystem="`uname -r | sed -E 's/^.*-(.*)$/\1/'`"
+            if [ `uname` == "Darwin" ]; then
+                export ETROBO_OS="mac"
+                export ETROBO_KERNEL="darwin"
+                export ETROBO_KERNEL_POSTFIX="mac"
+                export ETROBO_USERPROFILE="$HOME_ORG"
+                export ETROBO_EXE_POSTFIX=".app"
+            elif [ "$subsystem" == "Microsoft" ] || [ "$subsystem" == "WSL2" ]; then
+                if [ "$subsystem" == "Microsoft" ]; then
+                    subsystem="WSL1"
+                fi
+                export ETROBO_OS="win"
+                export ETROBO_OS_SUBSYSTEM="$subsystem"
+                export ETROBO_KERNEL="debian"
+                export ETROBO_KERNEL_POSTFIX="linux"
+                comspec="`which cmd.exe`"
+                if [ -z "$comspec" ]; then
+                    comspec="/mnt/c/Windows/System32/cmd.exe"
+                    export ETROBO_MODE_CUI="true"
+                fi
+                mntc="/mnt/$($comspec /c echo %USERPROFILE% 2>/dev/null | sed -E 's/.*/\L&/' | sed -E 's/^(.{1}).*$/\1/')"
+                uppath="$($comspec /c echo %USERPROFILE% 2>/dev/null | sed -r 's/^(.{1}):(.*)$/\2/' | sed -r 's/:|\r|\n//g' | sed -r 's/\\/\//g')"
+                export ETROBO_USERPROFILE="$mntc$uppath"
+                if [ -z "$uppath" ]; then
+                    export ETROBO_USERPROFILE="/mnt/c/Users/`whoami`"
+                fi
+                export ETROBO_EXE_POSTFIX=".exe"
+            elif [ "`ls /mnt/chromeos > /dev/null 2>&1; echo $?`" = "0" ]; then
+                export ETROBO_OS="chrome"
+                export ETROBO_KERNEL="debian"
+                export ETROBO_KERNEL_POSTFIX="linux"
+                export ETROBO_USERPROFILE="$HOME"
+                export ETROBO_EXE_POSTFIX=".$ETROBO_PLATFORM"
+            elif [ `uname` == "Linux" ]; then
+                export ETROBO_OS="linux"
+                export ETROBO_KERNEL="debian"
+                export ETROBO_KERNEL_POSTFIX="linux"
+                export ETROBO_USERPROFILE="$HOME"
+                export ETROBO_EXE_POSTFIX=".$ETROBO_PLATFORM"
             fi
-            export ETROBO_OS="win"
-            export ETROBO_OS_SUBSYSTEM="$subsystem"
-            export ETROBO_KERNEL="debian"
-            export ETROBO_KERNEL_POSTFIX="linux"
 
-            echo
-            echo "WSL2 debug: etroboenv.sh $quit"
-            echo "WSL2 debug: detected OS=$ETROBO_OS($ETROBO_OS_SUBSYSTEM) KERNEL=$ETROBO_KERNEL/$ETROBO_KERNEL_POSTFIX "
+            export ETROBO_ATHRILL_TARGET="$ETROBO_ROOT/athrill-target-v850e2m/build_${ETROBO_KERNEL_POSTFIX}"
 
-            comspec="`which cmd.exe`"
-            echo "WSL2 debug: COMSPEC detector 1st path: $comspec"
-            if [ -z "$comspec" ]; then
-                comspec="/mnt/c/Windows/System32/cmd.exe"
-                export ETROBO_MODE_CUI="true"
+            . "$ETROBO_SCRIPTS/etrobopath.sh" unset
+            export ETROBO_ENV_VER=`cd "$ETROBO_ROOT"; git show -s --date=short --format="%cd.%h"`
+            . "$ETROBO_SCRIPTS/etrobopath.sh"
+
+            # import module envs
+            . "$ETROBO_SCRIPTS/sim" env
+            . "$ETROBO_SCRIPTS/etrobopkg" env
+            . "$ETROBO_SCRIPTS/etrobopath.sh"
+
+            if [ "$BEERHALL_INVOKER" != "booting" ] && [ -z "$quit" ]; then
+                echo
+                echo "etrobo environment: Ready. (Ver.$ETROBO_ENV_VER)"
+                echo
+                echo 'to disable this environment, run `touch $ETROBO_ROOT/disable` and restart terminal'
+                echo
             fi
-            echo "WSL2 debug: COMSPEC detector 2nd path: $comspec"
-            echo "WSL2 debug: USERPROFILE detector 1st path: $($comspec /c echo %USERPROFILE%)"
-            mntc="/mnt/$($comspec /c echo %USERPROFILE% 2>/dev/null | sed -E 's/.*/\L&/' | sed -E 's/^(.{1}).*$/\1/')"
-            echo "WSL2 debug: USERPROFILE drive detector: $mntc"
-            uppath="$($comspec /c echo %USERPROFILE% 2>/dev/null | sed -r 's/^(.{1}):(.*)$/\2/' | sed -r 's/:|\r|\n//g' | sed -r 's/\\/\//g')"
-            echo "WSL2 debug: USERPROFILE parser: $($comspec /c echo %USERPROFILE% 2>/dev/null | sed -r 's/^(.{1}):(.*)$/\2/')"
-            echo "WSL2 debug: USERPROFILE detector 2nd path: $uppath"
-
-            export ETROBO_USERPROFILE="$mntc$uppath"
-            if [ -z "$uppath" ]; then
-                echo "WSL2 debug: USERPROFILE detector: fallback to using whoami"
-                export ETROBO_USERPROFILE="/mnt/c/Users/`whoami`"
-            fi
-            echo "WSL2 debug: USERPROFILE detector 3rd path: $ETROBO_USERPROFILE"
-            #export ETROBO_LAUNCH_SIM='cmd.exe /c "%USERPROFILE%\\etrobosim${ETROBO_SIM_VER}_${ETROBO_OS}\\${ETROBO_SIM_NAME}${ETROBO_EXE_POSTFIX}" &'
-            export ETROBO_EXE_POSTFIX=".exe"
-        elif [ "`ls /mnt/chromeos > /dev/null 2>&1; echo $?`" = "0" ]; then
-            export ETROBO_OS="chrome"
-            export ETROBO_KERNEL="debian"
-            export ETROBO_KERNEL_POSTFIX="linux"
-            export ETROBO_USERPROFILE="$HOME"
-            export ETROBO_EXE_POSTFIX=".$ETROBO_PLATFORM"
-        elif [ `uname` == "Linux" ]; then
-            export ETROBO_OS="linux"
-            export ETROBO_KERNEL="debian"
-            export ETROBO_KERNEL_POSTFIX="linux"
-            export ETROBO_USERPROFILE="$HOME"
-            export ETROBO_EXE_POSTFIX=".$ETROBO_PLATFORM"
-        fi
-
-        export ETROBO_ATHRILL_TARGET="$ETROBO_ROOT/athrill-target-v850e2m/build_${ETROBO_KERNEL_POSTFIX}"
-
-        . "$ETROBO_SCRIPTS/etrobopath.sh" unset
-        export ETROBO_ENV_VER=`cd "$ETROBO_ROOT"; git show -s --date=short --format="%cd.%h"`
-        . "$ETROBO_SCRIPTS/etrobopath.sh"
-
-        # import module envs
-        . "$ETROBO_SCRIPTS/sim" env
-        . "$ETROBO_SCRIPTS/etrobopkg" env
-        . "$ETROBO_SCRIPTS/etrobopath.sh"
-
-        if [ "$BEERHALL_INVOKER" != "booting" ] && [ -z "$quit" ]; then
+        else
             echo
-            echo "etrobo environment: Ready. (Ver.$ETROBO_ENV_VER)"
+            echo "etrobo environment: Disabled."
             echo
-            echo 'to disable this environment, run `touch $ETROBO_ROOT/disable` and restart terminal'
+            echo 'to re-enable this environment, run `rm $ETROBO_ROOT/disable` and restart terminal'
             echo
         fi
-    else
-        echo
-        echo "etrobo environment: Disabled."
-        echo
-        echo 'to re-enable this environment, run `rm $ETROBO_ROOT/disable` and restart terminal'
-        echo
+    elif [ -z "$quit" ]; then
+        echo "etrobo environment: Ready. (Ver.$ETROBO_ENV_VER)"
     fi
 fi
 
